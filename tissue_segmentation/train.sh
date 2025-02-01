a95063f44950c1f85acfa5f3b30255fca2b8aa44
Kang Won Byun <69283127+Kang812@users.noreply.github.com>
2025-02-02 02:31:19 +0900

    Add files via upload

---
+++ tissue_segmentation/train.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0,1 PORT=29500 /workspace/whole_slide_image_LLM/mmsegmentation/tools/dist_train.sh /workspace/whole_slide_image_LLM/wsi_level_vqa-main/tissue_segmentation/config/pidnet_cfg.py 2
